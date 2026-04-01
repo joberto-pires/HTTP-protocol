8faeabc2ca49bb439b766df4f69ea788e6dca118
keita <jopiresdev@gmail.com>
2026-04-01 10:15:10 -0400

    testes para o parse dos headers

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -11,6 +11,27 @@ type Headers struct {
 
 var endLine = []byte("\r\n")
 var badfdline = fmt.Errorf("malformed field-line!")
+var badfdname = fmt.Errorf("malformed field-name!")
+
+func ParseHeaders(data []byte) (string, string, error) {
+	head  :=bytes.SplitN(data, []byte(":"), 2)
+	if len(head) != 2 {
+		return "", "", badfdline
+	}
+	if bytes.HasSuffix(head[0], []byte(" ")) {
+		return "", "", badfdname
+	}
+	// Header = head[0]: head[1] \r\n \r\n
+	fieldName := head[0]
+	fieldValue := bytes.TrimSpace(head[1])
+	return string(fieldName), string(fieldValue), nil
+
+
+	
+}
+
+func newHeaders() {
+}
 
 func (h Headers) Parse(data []byte) (int, bool, error) {
 	idx := bytes.Index(data, endLine)
@@ -21,15 +42,12 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 	if idx == 0 {
 		return idx, true, nil
 	}
-	head  :=bytes.SplitN(data, []byte(":"), 2)
-	if len(head) != 2 {
+	name, value, err := ParseHeaders(data[:idx])
+	if err != nil {
 		return 0, false, badfdline
 	}
-	// Header = head[0]: head[1] \r\n \r\n
-	//fieldName := head[0]
-	//fieldValue := bytes.TrimSpace(head[1])
-
-	return 0, false, nil
+	n := idx + len(endLine)
+	return n, false, nil
 
 
 }
